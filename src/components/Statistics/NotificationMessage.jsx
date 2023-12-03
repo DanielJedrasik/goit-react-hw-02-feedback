dec6ba539ab0f8d01a9a02ea4d7e07278ca2f825
DanielJedrasik <djedrasik3@gmail.com>
2023-12-03 19:17:12 +0100

    update 3

---
+++ src/components/Statistics/NotificationMessage.jsx
@@ -0,0 +1,7 @@
+import css from './Statistics.module.css';
+
+const NotificationMessage = () => {
+  return <p className={css.feedback}>There is no feedback</p>;
+};
+
+export default NotificationMessage;
